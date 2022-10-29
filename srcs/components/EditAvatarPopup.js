d4139453273db02053a00d82af8cc342567917a2
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2022-10-29 21:24:07 +0300

    Add files via upload

---
+++ srcs/components/EditAvatarPopup.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import PopupWithForm from './PopupWithForm.js'
+
+function EditAvatarPopup({isClose, isOpen, onUpdateAvatar}) {
+    const [avatar, setAvatar] = useState('')
+
+    function handleChangeAvatar(e) {
+        setAvatar(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        onUpdateAvatar(avatar)
+    }
+
+    return(
+        <PopupWithForm name="avatar" title="Обновить аватар" id="" isClose={ isClose } isOpen={ isOpen } btnText="Сохранить" onSubmit={handleSubmit}>
+          <div className="popup__input-container">
+            <input className="popup__input" placeholder="Ссылка на аватар" value={avatar} type="url"  required name="link" onChange={handleChangeAvatar}/>
+            <p className="popup__input-error-text"></p>
+          </div>
+      </PopupWithForm>
+    )
+
+
+}
+
+export default EditAvatarPopup;
